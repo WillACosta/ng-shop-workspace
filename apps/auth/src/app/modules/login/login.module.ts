7f21f688f4115a5426327ad2e257558774259f31
willACosta <williamamaral521@gmail.com>
2023-09-17 16:17:01 -0300

    feat: implement form and validations for login UI

--- apps/auth/src/app/modules/login/login.module.ts
+++ apps/auth/src/app/modules/login/login.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { LoginViewComponent } from './views';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forChild([
       {
         path: '',
--- apps/auth/src/app/modules/login/views/login-view.component.ts
+++ apps/auth/src/app/modules/login/views/login-view.component.ts
@@ -1,7 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'ng-shop-workspace-login-view',
-  templateUrl: 'login-view.component.html',
+  templateUrl: 'login-view.component.html'
 })
-export class LoginViewComponent {}
+export class LoginViewComponent implements OnInit {
+  loginForm: FormGroup<{
+    email: FormControl<string | null>
+    password: FormControl<string | null>
+  }>
+
+  constructor(private readonly fb: FormBuilder) {}
+
+  ngOnInit() {
+    this._buildForm()
+  }
+
+  onClickSignIn() {
+    if (!this.loginForm.valid) {
+      return this.loginForm.markAllAsTouched()
+    }
+
+    const { email, password } = this.loginForm.value
+    console.log({ email, password })
+  }
+
+  onClickForgottenPassword() {
+    // TODO:
+  }
+
+  private _buildForm() {
+    this.loginForm = this.fb.group({
+      email: ['', Validators.compose([Validators.email, Validators.required])],
+      password: ['', Validators.required]
+    })
+  }
+}
