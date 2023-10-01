241c80aea7bcfa0c2313b2d65c9507b9cbe0d4ab
willACosta <williamamaral521@gmail.com>
2023-09-30 21:16:41 -0300

    fix: change auth guard injection classes

--- apps/shell/src/app/core/guards/auth.guard.ts
+++ apps/shell/src/app/core/guards/auth.guard.ts
@@ -1,13 +1,16 @@
 import { Router } from '@angular/router'
-import { AuthFacade } from '@ng-shop-workspace/auth-state'
+import { AuthState } from '@ng-shop-workspace/auth-state'
 
 import { inject } from '@angular/core'
+import { Store } from '@ngxs/store'
 
 export function authGuard() {
   const router = inject(Router)
-  const authFacadeService = inject(AuthFacade)
+  // TODO: why this not work? const authFacadeService = inject(AuthFacade)
+  const store = inject(Store)
 
-  if (authFacadeService.isAuthenticated) return true
+  const isAuthenticated = store.selectSnapshot(AuthState.isAuthenticated)
+  if (isAuthenticated) return true
 
   router.navigate(['/auth'])
   return false
