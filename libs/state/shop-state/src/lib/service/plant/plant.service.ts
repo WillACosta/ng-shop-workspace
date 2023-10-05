86adfb9eb39d57466424daa83a7375534eed174a
willACosta <williamamaral521@gmail.com>
2023-10-05 10:31:46 -0300

    docs: update screenshot image

--- libs/state/shop-state/src/lib/service/plant/plant.service.ts
+++ libs/state/shop-state/src/lib/service/plant/plant.service.ts
@@ -5,22 +5,25 @@ import { retry } from 'rxjs'
 const BASE_URL = 'http://localhost:3000'
 
 interface ApiResponseOf<T> {
-  data: Array<T>
+	data: Array<T>
 }
 
 interface PlantResponse {
-  name: string
-  price: number
-  description: string
-  image: string
+	name: string
+	price: number
+	description: string
+	image: string
 }
 
 @Injectable()
 export class PlantService {
-  constructor(private _http: HttpClient) {}
+	constructor(private _http: HttpClient) {}
 
-  loadAllPlants() {
-    return this._http.get<ApiResponseOf<PlantResponse>>(`${BASE_URL}/plants`)
-    .pipe(retry(2))
-  }
+	private _lang = 'en'
+
+	loadAllPlants() {
+		return this._http
+			.get<ApiResponseOf<PlantResponse>>(`${BASE_URL}/plants-${this._lang}`)
+			.pipe(retry(2))
+	}
 }
